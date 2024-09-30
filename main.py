323552f6fdbe9850b2255bebc6c7001f580b1b21
Escalante Axel Joel <axelescalante0@gmail.com>
2024-09-30 11:02:38 -0300

    APUNTES.

--- main.py
+++ main.py
@@ -1,32 +1,67 @@
+#Desarrollo de script para cruzar los datos de la bibilioteca con el proviemiente en la catedra.
+#planiacion del script
 
-import pandas as pd
-from src.lectura import leer_csv
+#1. Puedo aplicar un ordenamiento rapido (quick sort) a los datos de la catedra y de la biblioteca(ordenar por ISBN). Esto para luego aplicar busqueda binaria.
+#2. Aplicar busqueda binaria para buscar los datos de la catedra en la biblioteca.
 
-#libros de material de consulta
-df_libros_consulta = pd.read_csv('Archivos/libros (mat de consulta) total en Koha al 2_9_2024.csv', delimiter=';') 
-#libros total en koha 
-df_libros_total = pd.read_csv('Archivos/ANALISIS DE COLECCIÓN - PRUEBA -reportresults.csv', delimiter=';')
-#libros en bidi
-df_libros_bidi = pd.read_csv('Archivos/libros vigentes en bidi al 2_9_2024.csv', delimiter=';')
+#Los datos resultante son de todos los años, los de la caatedra son de primer año. ¿Como puedo filtrar los datos de la biblioteca para que solo sean de primer año?--pensasr mas adelante.
 
-#-------------------------------------------------------------------------------------------------------------------------------
-#BASE DE DATOS BIDI
-# Asignar nombres de columnas a la base de datos bidi ya que no cuenta con esto
-df_libros_bidi.columns = ['ID', 'Titulo', 'Autor', 'isbn', 'Editorial','Universidad','Numero_licencia','dato','Fecha_vencimiento_lic'] 
-#print(df_libros_bidi.head())
-df_libros_bidi_ = df_libros_bidi[['ID', 'Titulo', 'Autor', 'isbn', 'Editorial','Fecha_vencimiento_lic']]
-print(df_libros_bidi_.head())
-
-#-------------------------------------------------------------------------------------------------------------------------------
-#BASE DE DATOS SOBRE EL MATERIAL DE CONSULTA KOHA
-print(df_libros_consulta.head())
-df_libros_consulta_ = df_libros_consulta[['barcode','title','author','isbn']]
-print(df_libros_consulta_.head(50))
-
-
-#-------------------------------------------------------------------------------------------------------------------------------
-#BASE DE DATOS TOTAL KOHA
-print('aaaaaaa')
-print(df_libros_total.head())
-df_libros_total_ = df_libros_total[['barcode','title','author','isbn']]
-print(df_libros_total_.head(50))
+def swap(A, i, j):
+ 
+    temp = A[i]
+    A[i] = A[j]
+    A[j] = temp
+ 
+ 
+# Partición # utilizando el esquema de partición Lomuto
+def partition(a, start, end):
+ 
+    # Elija el elemento más a la derecha como pivote de la lista
+    pivot = a[end]
+ 
+    # Los elementos # menores que el pivote serán empujados a la izquierda de `pIndex`
+    # Los elementos # más que el pivote se empujarán a la derecha de `pIndex`
+    # elementos iguales pueden ir en cualquier dirección
+    pIndex = start
+ 
+    # cada vez que encontramos un elemento menor o igual que el pivote,
+    # `pIndex` se incrementa, y ese elemento se colocaría
+    # antes del pivote.
+    for i in range(start, end):
+        if a[i] <= pivot:
+            swap(a, i, pIndex)
+            pIndex = pIndex + 1
+ 
+    # swap `pIndex` con pivote
+    swap(a, end, pIndex)
+ 
+    # devuelve `pIndex` (índice del elemento pivote)
+    return pIndex
+ 
+ 
+# Rutina de clasificación rápida
+def quicksort(a, start, end):
+ 
+    # Condición base
+    if start >= end:
+        return
+ 
+    # reorganizar elementos a través de pivote
+    pivot = partition(a, start, end)
+ 
+    # recurre en la sublista que contiene elementos menores que el pivote
+    quicksort(a, start, pivot - 1)
+ 
+    # recurre en la sublista que contiene más elementos que el pivote
+    quicksort(a, pivot + 1, end)
+ 
+ 
+# Implementación en Python del algoritmo Quicksort
+if __name__ == '__main__':
+ 
+    a = [9, -3, 5, 2, 6, 8, -6, 1, 3]
+ 
+    quicksort(a, 0, len(a) - 1)
+ 
+    # imprime la lista ordenada
+    print(a)
