54a85e6f4580a7135cdbf003549fe12da8156414
Escalante Axel Joel <axelescalante0@gmail.com>
2024-10-04 22:24:35 -0300

    Algoritmo de buscardor de Titulo y Autor

---
+++ src/buscador.py
@@ -0,0 +1,42 @@
+#modulo para buscar libros por titulo, autor o ISBN
+import sys
+import os
+
+sys.path.append(os.path.abspath('C:/Users/Axel/Desktop/Practica-Biblioteca'))
+
+import pandas as pd
+
+csv = 'C:/Users/Axel/Desktop/Practica-Biblioteca/data/procesados/libros_biblioteca.csv'
+df = pd.read_csv(csv, delimiter=',')
+
+def buscar_libro(df, columna, valor):
+    """
+    Busca un libro en la base de datos de la biblioteca.
+
+    Parámetros:
+    -----------
+    df : pandas.DataFrame
+        DataFrame con los datos de la biblioteca.
+    columna : str
+        Nombre de la columna por la cual se buscará el libro.
+    valor : str
+        Valor a buscar en la columna especificada.
+
+    Returns:
+    --------
+    pandas.DataFrame
+        DataFrame con los datos del libro encontrado.
+    """
+    #pasar a minusculas el valor de la columna
+    df[columna] = df[columna].str.lower()
+    #pasar a minusculas el valor a buscar
+    valor = valor.lower()
+    #buscar el libro donde es un string, puedo buscar por titulo completo o por una parte del titulo
+    libro = df[df[columna].str.contains(valor, case=False, na=False)]
+    return libro
+
+
+#eje de prueba
+libro = buscar_libro(df, 'author', 'CARLINO')
+
+print(libro)
