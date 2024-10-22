89eafe8b3dd107faf6189813d48bd926f43ead3c
Escalante Axel Joel <axelescalante0@gmail.com>
2024-10-22 11:05:39 -0300

    Cambios en bilbiografia procesada. Se modificaron el formato de los autores a uno estandar

--- src/buscador.py
+++ src/buscador.py
@@ -37,6 +37,8 @@ def buscar_libro(df, columna, valor):
 
 
 #eje de prueba
-libro = buscar_libro(df, 'author', 'grossman')
+libro = buscar_libro(df, 'title', 'Tratado de derecho')
 
 print(libro)
+
+   
